2f9380a5bada8d6e210f74b64da8b6c8df2cba68
mdmohsin2 <mohsinbd862554@gmail.com>
2023-05-29 16:48:28 +0600

    navbar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,227 +10,151 @@ export default function Navbar() {
     return <div>
 
 
-
-        {/* <div onClick={() => setOpen(!open)} className="lg:hidden drawer-content bg-gray-100">
-            <label>
-                {
-                    open ? <BsList className=" h-8 w-8 cursor-pointer" /> : <BsList className=" h-8 w-8 cursor-pointer" />
-                }
-            </label>
-        </div>
-
-        <div onClick={() => setOpen(!open)} className="lg:hidden">
-            <label>
-                {
-                    open ? <BsXLg className=" h-8 w-8 -mt-6 cursor-pointer" /> : <BsXLg className="h-8 w-8 hidden" />
-                }
-            </label>
-        </div> */}
-
-
-
-        <nav className="flex items-center justify-around text-blue-700">
-
-            {/* logo */}
+        {/* drawer button */}
+        <div className='flex items-center justify-between bg-gray-200 lg:hidden'>
             <div className="avatar">
-                <div className="w-14 lg:w-24 mb-5 lg:mb-0">
+                <div className="w-14">
                     <img src={`https://i.ibb.co/wpwXr0W/Logo-1.png`} alt="" />
                 </div>
             </div>
-
-            {/* menu */}
-            <div className={style.fontColor}>
-                <menu className="text-sm font-bold uppercase">
-                    <ul className="flex items-center space-x-4">
-                        <li><Link href=''>Home</Link></li>
-                        <li><Link href=''>About</Link></li>
-                        <li className={style.drops}><Link href=''>Diocesen Commissions  &#10095;</Link>
-                            <ul className={style.drop}>
-                                <li><Link href=''>Barishal</Link></li>
-                                <li><Link href=''>Chattogram</Link></li>
-                                <li><Link href=''>Dhaka</Link></li>
-                                <li><Link href=''>Dinajpur</Link></li>
-                                <li><Link href=''>Khulna</Link></li>
-                                <li><Link href=''>Mymensingh</Link></li>
-                                <li><Link href=''>Rajshahi</Link></li>
-                                <li><Link href=''>Sylhet</Link></li>
-                            </ul>
-                        </li>
-                        <li className={style.drops}><Link href=''>Movements &#10095;</Link>
-                            <ul className={style.drop2}>
-                                <li><Link href=''>Bangladesh Catholic Students’ Movement (BCSM)</Link></li>
-                                <li><Link href=''>Young Christian Students (YCS) & Young Students Movement (YSM)</Link></li>
-                                <li><Link href=''>Jesus Youth </Link></li>
-                            </ul>
-                        </li>
-                        <li><Link href=''>Blog</Link></li>
-                        <li className={style.drops}><Link href=''>RESOURCES &#10095;</Link>
-                        <ul className={style.drop}>
-                                <li><Link href=''>Gallery</Link></li>
-                                <li className={style.dropLast}><Link href=''>Publications  &#10095;</Link>
-                                    <ul className={style.drop3}>
-                                        <li><Link href=''>Newsletter</Link></li>
-                                        <li><Link href=''>Nobokollol</Link></li>
-                                        <li><Link href=''>BCSM BARTA</Link></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li><Link href=''>Contact</Link></li>
-                    </ul>
-                </menu>
+            <div onClick={() => setOpen(!open)} className="lg:hidden drawer-content">
+                <label htmlFor="my-drawer-2" className='drawer-button'>
+                    {
+                        open ? <BsXLg className=" h-8 w-8 cursor-pointer" /> : <BsList className=" h-8 w-8 cursor-pointer" />
+                    }
+                </label>
             </div>
-
-            {/* button */}
-            <div>
-                <button className="btn btn-sm bg-red-500">Contact Us</button>
-            </div>
-        </nav>
+        </div>
 
 
 
 
+        {/* screen size desktop */}
+        <div className='hidden lg:block'>
+            <nav className="flex items-center justify-around text-blue-700">
 
-
-
-        {/* <nav className="hidden lg:block">
-            <div className="lg:flex items-center justify-around ">
-               
+                {/* logo */}
                 <div className="avatar">
                     <div className="w-14 lg:w-24 mb-5 lg:mb-0">
                         <img src={`https://i.ibb.co/wpwXr0W/Logo-1.png`} alt="" />
                     </div>
                 </div>
 
-              
-                <div>
-                    <ul className="lg:flex items-center lg:space-x-4 text-sm font-bold">
-                        <li className="border-b-2 border-gray-600 lg:border-0"><Link href=''>Home</Link></li>
-                        <li className="border-b-2 border-gray-600 lg:border-0"><Link href=''>About</Link></li>
-                        <div className="dropdown ">
-                            <li tabIndex={0} className=" border-b-2 border-gray-600 lg:border-0 w-full block"><Link href=''>Diocesen Commissions  &#10095;</Link>
-                                <ul tabIndex={0} className="menu dropdown-content pt-9  bg-gray-100 w-full rounded-md">
+                {/* menu */}
+                <div className={style.fontColor}>
+                    <menu className="text-sm font-bold uppercase">
+                        <ul className="flex items-center space-x-4">
+                            <li><Link href='/'>Home</Link></li>
+                            <li><Link href='/about'>About</Link></li>
+                            <li className={style.drops}><Link href=''>Diocesen Commissions  &#10095;</Link>
+                                <ul className={style.drop}>
+                                    <li><Link href=''>Barishal</Link></li>
+                                    <li><Link href=''>Chattogram</Link></li>
                                     <li><Link href=''>Dhaka</Link></li>
-                                    <li><Link href=''>Cumilla</Link></li>
-                                    <li><Link href=''>Sylhet</Link></li>
-                                    <li><Link href=''>Rajshahi</Link></li>
+                                    <li><Link href=''>Dinajpur</Link></li>
                                     <li><Link href=''>Khulna</Link></li>
+                                    <li><Link href=''>Mymensingh</Link></li>
+                                    <li><Link href=''>Rajshahi</Link></li>
+                                    <li><Link href=''>Sylhet</Link></li>
                                 </ul>
                             </li>
-                        </div>
-
-                        <div className="dropdown ">
-                            <li tabIndex={0} className=" border-b-2 border-gray-600 lg:border-0 block"><Link href=''>Movements &#10095;</Link>
-                                <ul tabIndex={0} className="menu dropdown-content  bg-gray-100 w-80 rounded-md pt-9">
+                            <li className={style.drops}><Link href=''>Movements &#10095;</Link>
+                                <ul className={style.drop2}>
                                     <li><Link href=''>Bangladesh Catholic Students’ Movement (BCSM)</Link></li>
                                     <li><Link href=''>Young Christian Students (YCS) & Young Students Movement (YSM)</Link></li>
                                     <li><Link href=''>Jesus Youth </Link></li>
                                 </ul>
                             </li>
-                        </div>
-
-                        <li className="border-b-2 border-gray-600 lg:border-0"><Link href=''>BLOG</Link></li>
-                        <div className="dropdown ">
-                            <li tabIndex={1} className=" border-b-2 border-gray-600 lg:border-0 block "><Link href=''>RESOURCES &#10095;</Link>
-                                <ul tabIndex={1} className="menu dropdown-content  bg-gray-100 w-64 rounded-md pt-9">
-                                    <li><Link href=''>Gallery </Link></li>
-                                    <li><Link href=''>Publications &#10095;</Link>
-                                        <ul className="bg-gray-100">
+                            <li><Link href='/blog'>Blog</Link></li>
+                            <li className={style.drops}><Link href=''>RESOURCES &#10095;</Link>
+                                <ul className={style.drop}>
+                                    <li><Link href=''>Gallery</Link></li>
+                                    <li className={style.dropLast}><Link href=''>Publications  &#10095;</Link>
+                                        <ul className={style.drop3}>
                                             <li><Link href=''>Newsletter</Link></li>
                                             <li><Link href=''>Nobokollol</Link></li>
                                             <li><Link href=''>BCSM BARTA</Link></li>
-                                        </ul></li>
+                                        </ul>
+                                    </li>
                                 </ul>
                             </li>
-                        </div>
-                        <li className="border-b-2 border-gray-600 lg:border-0"><Link href=''>Contact</Link></li>
-                    </ul>
+                            <li><Link href='/contact'>Contact</Link></li>
+                        </ul>
+                    </menu>
                 </div>
 
-              
-                <div className="btn btn-sm bg-red-600">Contact us</div>
-            </div>
-        </nav> */}
+                {/* button */}
+                <div>
+                    <button className="btn btn-sm bg-red-500">Contact Us</button>
+                </div>
+            </nav>
+        </div>
 
 
-        {/* <div className="absolute z-50 lg:hidden">
 
-            <div className="drawer">
-                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-side lg:hidden overflow-hidden">
-                    <label htmlFor="my-drawer" className="drawer-overlay"></label>
+
+        {/* mobile size */}
+        <div className='lg:hidden absolute z-20'>
+            <div className="drawer drawer-mobile">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col items-center justify-center">
+                    {/* Page content here */}
+
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 text-base-content bg-gray-100">
-
-                        <nav className="">
-                            <div className="lg:flex items-center justify-around ">
-
-                                
-                                <div className="flex justify-between items-center">
-                                    <div className="avatar">
-                                        <div className="w-14 lg:w-24 mb-5 lg:mb-0">
-                                            <img src={`https://i.ibb.co/wpwXr0W/Logo-1.png`} alt="" />
-                                        </div>
-                                    </div>
-                                    <div onClick={() => setOpen(!open)} className="lg:hidden">
-                                        <label htmlFor="my-drawer" className="drawer-button">
-                                            {
-                                                open ? <BsXLg className=" h-8 w-8 -mt-6 cursor-pointer" /> : <BsXLg className="h-8 w-8 hidden" />
-                                            }
-                                        </label>
-                                    </div>
-                                </div>
-
-
-                               
-                                <div>
-                                    <ul className="lg:flex items-center lg:space-x-4 text-sm font-bold">
-                                        <li className="border-b-2 border-gray-600 lg:border-0"><Link href=''>Home</Link></li>
-                                        <li className="border-b-2 border-gray-600 lg:border-0"><Link href=''>About</Link></li>
-                                        <div className="dropdown border-b-2 border-gray-600 lg:border-0 w-full">
-                                            <li tabIndex={0} className=""><Link href=''>Diocesen Commissions  &#10095;</Link>
-                                                <ul tabIndex={0} className="menu dropdown-content  bg-gray-100 w-full rounded-md">
-                                                    <li><Link href=''>Dhaka</Link></li>
-                                                    <li><Link href=''>Cumilla</Link></li>
-                                                    <li><Link href=''>Sylhet</Link></li>
-                                                    <li><Link href=''>Rajshahi</Link></li>
-                                                    <li><Link href=''>Khulna</Link></li>
-                                                </ul>
-                                            </li>
-                                        </div>
-                                        <div className="dropdown border-b-2 border-gray-600 lg:border-0 w-full">
-                                            <li tabIndex={0} className=""><Link href=''>Movements &#10095;</Link>
-                                                <ul tabIndex={0} className="menu dropdown-content  bg-gray-100 w-full rounded-md">
-                                                    <li><Link href=''>Bangladesh Catholic Students’ Movement (BCSM)</Link></li>
-                                                    <li><Link href=''>Young Christian Students (YCS) & Young Students Movement (YSM)</Link></li>
-                                                    <li><Link href=''>Jesus Youth </Link></li>
-                                                </ul>
-                                            </li>
-                                        </div>
-                                        <li className="border-b-2 border-gray-600 lg:border-0"><Link href=''>BLOG</Link></li>
-                                        <li className="border-b-2 border-gray-600 lg:border-0"><Link href=''>RESOURCES &#10095;</Link>
-                                            <ul >
-                                                <li><Link href=''>Gallery</Link></li>
-                                                <li><Link href=''>Publications &#10095;</Link>
-                                                    <ul tabIndex={0} className="menu dropdown-content  bg-gray-100 w-full rounded-md">
+                        <nav>
+                            {/* mobile size navbar */}
+                            <div className='text-blue-600 uppercase text-sm font-bold'>
+                                <ul>
+                                    <li><Link href=''>Home</Link></li>
+                                    <li><Link href=''>About</Link></li>
+                                    <li className={style.dropsMobile}><Link href=''>Diocesen Commissions  &#10095;</Link>
+                                        <ul className={style.dropMobile}>
+                                            <li><Link href=''>Barishal</Link></li>
+                                            <li><Link href=''>Chattogram</Link></li>
+                                            <li><Link href=''>Dhaka</Link></li>
+                                            <li><Link href=''>Dinajpur</Link></li>
+                                            <li><Link href=''>Khulna</Link></li>
+                                            <li><Link href=''>Mymensingh</Link></li>
+                                            <li><Link href=''>Rajshahi</Link></li>
+                                            <li><Link href=''>Sylhet</Link></li>
+                                        </ul>
+                                    </li>
+                                    <li className={style.dropsMobile}><Link href=''>Movements &#10095;</Link>
+                                        <ul className={style.dropMobile}>
+                                            <li><Link href=''>Bangladesh Catholic Students’ Movement (BCSM)</Link></li>
+                                            <li><Link href=''>Young Christian Students (YCS) & Young Students Movement (YSM)</Link></li>
+                                            <li><Link href=''>Jesus Youth </Link></li>
+                                        </ul>
+                                    </li>
+                                    <li><Link href=''>Blog</Link></li>
+                                    <li className={style.dropsMobile}><Link href=''>RESOURCES &#10095;</Link>
+                                        <ul className={style.dropMobile}>
+                                            <li><Link href=''>Gallery</Link></li>
+                                            <li className={style.dropsMobile2}><Link href=''>Publications  &#10095;</Link>
+                                                <div className={style.dropMobile2}>
+                                                    <ul className='bg-none'>
                                                         <li><Link href=''>Newsletter</Link></li>
                                                         <li><Link href=''>Nobokollol</Link></li>
                                                         <li><Link href=''>BCSM BARTA</Link></li>
                                                     </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li className="border-b-2 border-gray-600 lg:border-0"><Link href=''>Contact</Link></li>
-                                    </ul>
-                                </div>
-
-
-                                <div className="btn btn-sm bg-red-600 mt-2">Contact us</div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><Link href=''>Contact</Link></li>
+                                </ul>
+                            </div>
+                            <div className='mt-4'>
+                                <button className="btn btn-sm bg-red-500">Contact Us</button>
                             </div>
                         </nav>
-
                     </ul>
                 </div>
             </div>
-        </div> */}
+        </div>
+
     </div >
 }
