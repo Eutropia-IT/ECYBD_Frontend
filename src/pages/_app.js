5c8df85d3e729f182cf26f76eef8419bfa0e018c
tahmid <tahmidshovon30@gmail.com>
2023-07-08 04:58:26 +0600

    cleaned

--- src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  const getLayout = Component.getLayout ?? ((page) => page)
-  return  getLayout(<Component {...pageProps}/>)
-}
